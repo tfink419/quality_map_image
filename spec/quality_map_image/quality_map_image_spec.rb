f5ac16b9537ad417dc2563ff751c6bd0e9cd5381
Tyler Fink <tfink419@gmail.com>
2020-07-24 18:35:45 -0600

    Fixing test to look like it used to

--- spec/quality_map_image/quality_map_image_spec.rb
+++ spec/quality_map_image/quality_map_image_spec.rb
@@ -14,14 +14,14 @@ QUALITY_OF_POINT_POLYGONS_WITH_HOLE=[
 
 QUALITY_OF_POINTS_CENSUS_TRACT_LAT = 36.9*524288/180
 QUALITY_OF_POINTS_CENSUS_TRACT_LONG = -102.8*524288/180
-QUALITY_OF_POINTS_CENSUS_TRACT_WIDTH_HEIGHT = 350
+QUALITY_OF_POINTS_CENSUS_TRACT_WIDTH_HEIGHT = 291
 QUALITY_OF_POINTS_CENSUS_TRACT_POLYGONS = JSON.parse(File.read("spec/dumps/census_tract_polygon_dump"))
 QUALITY_OF_POINTS_CENSUS_TRACT_SCALE = 42000000
 
 QUALITY_OF_POINTS_SQUARE_LAT = 0
 QUALITY_OF_POINTS_SQUARE_LONG = 0
 QUALITY_OF_POINTS_SQUARE_SCALE = 200000000
-QUALITY_OF_POINTS_SQUARE_WIDTH_HEIGHT = 350
+QUALITY_OF_POINTS_SQUARE_WIDTH_HEIGHT = 291
 QUALITY_OF_POINTS_SQUARE_POLYGONS = [
   [[[[[0,0], [0, 0.05], [0.05, 0.05], [0.05, 0], [0, 0]]]], 10]
 ]
@@ -62,7 +62,7 @@ RSpec.describe QualityMapImage do
     image_data = [[0, 10, 0.5, QUALITY_OF_POINTS_CENSUS_TRACT_SCALE, true], [0, 10, 0.5, QUALITY_OF_POINTS_SQUARE_SCALE, false]]
     images << File.read("test_image.png")
     images << File.read("test_image2.png")
-    test_response = subject.get_image(350, images, image_data)
+    test_response = subject.get_image(291, images, image_data)
     expect(test_response).to be_truthy
     File.write("test_image3.png", test_response)
   end
@@ -72,7 +72,7 @@ RSpec.describe QualityMapImage do
     image_data = [[0, 40, 0.5, QUALITY_OF_POINTS_CENSUS_TRACT_SCALE, true], [0, 40, 0.5, QUALITY_OF_POINTS_SQUARE_SCALE, false]]
     images << File.read("test_image.png")
     images << File.read("test_image2.png")
-    test_response = subject.get_image(350, images, image_data)
+    test_response = subject.get_image(291, images, image_data)
     expect(test_response).to be_truthy
     File.write("test_image4.png", test_response)
   end
