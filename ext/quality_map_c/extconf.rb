53b7f217b8a79ff71e0041581fc69cc0840842c5
Tyler Fink <tfink419@gmail.com>
2020-07-25 22:41:48 -0600

    try both c and c++ libraries, try to print env

--- ext/quality_map_c/extconf.rb
+++ ext/quality_map_c/extconf.rb
@@ -2,9 +2,11 @@ require 'mkmf'
 
 abort 'Missing Glib Package.' unless pkg_config("glib-2.0")
 abort 'Missing Glib Object Package.' unless pkg_config("gobject-2.0")
-abort 'Missing VIPS Package' unless pkg_config("vips")
+abort 'Missing VIPS Package' unless pkg_config("vips") || pkg_config("vips-cpp")
 abort 'Missing PNG library.' unless have_library('png16')
 abort 'Missing Z library.' unless have_library('z')
+
+puts ENV
 
 LIBDIR      = RbConfig::CONFIG['libdir']
 INCLUDEDIR  = RbConfig::CONFIG['includedir']
